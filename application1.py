77d187f4adc70a603b4504eace504362b5beb149
suyash-20 <suyashschavan@gmail.com>
2020-12-14 15:42:33 +0530

    Revised the codebase as per Ulhas's recommended changes.

--- application1.py
+++ application1.py
@@ -1,52 +1,64 @@
 import csv
 
+from models import*
+
 book_file = open("books.csv",'rt')
-book_names = csv.reader(book_file)
+book_info = csv.reader(book_file)
 
 author_file = open("authors.csv",'rt')
 author_names = csv.reader(author_file)
 
+review_file = open("reviews.csv",'rt')
+book_reviews = csv.reader(review_file)
+
 
 def main():
-    book_list = []
-    count = 0
-    another_entry = 1
 
-    print("----------Welcome to the book store!----------")
-    print("________________________________________________\n")
+    #LISTS FOR STORING CSV FILE CONTENTS 
+    book_list = []
+    author_list = []
+    review_list = []
+    count = 0
     
-    print("Here's our catalogue to explore around:")
-    print("""
-    1. Books
-    2.Authors
-    3.Book Reviews
-    """)
+    #LOOPING VARIABLES
+    book_row_count = 4
+    author_row_count = 3
+    review_row_count = 3
+    
+    #WHILE LOOP VARIABLES
+    another_book = 1
+    another_author = 1
+    another_review = 1
+    
+    print_messages()
 
     choice = int(input("Enter your choice: "))
 
-    if choice ==1:
+    if choice == MainChoice.Books:
         print("""
-1. Browse through the book catalogue.
-2. Add books to the catalogue.
+        1. Browse through the book catalogue.
+        2. Add books to the catalogue.
 		""")
 		
-        user_input = int(input("Enter your choice as '1' or '2': "))
+        book_input = int(input("Enter your choice as '1' or '2': "))
         
-        if user_input == 1:
+        if book_input == 1:
             print("Here's our Book catalogue:")
-            
-            for row in book_names:
-                print("|\t",row[0],"|\t", row[1],"|\t", row[2],"|\t", row[3])
-                print("______________________________________________________")
+            for row in book_info:
+                book = Book(row[0],row[1],row[2],row[3])
+                book.print_books()
         
-        elif user_input == 2:
+        elif book_input == 2:
             
             print("""While making the entries, stick to the following order: 
-'Book_Id, Book_Name, Author_id, Year_of_publication'
+            Book_Id, 
+            Book_Name, 
+            Author_id,
+            Year_of_publication'
 			""")
             
-            while another_entry != 0:
-                while count<4:
+            while another_book != 0:
+                while count< book_row_count:
                     user_in = input("Enter the entries for books: ")
                     book_list.append(user_in)
                     count += 1
@@ -55,14 +67,70 @@ def main():
                     writer = csv.writer(csvfile)
                     writer.writerow(book_list)
                 
-                another_entry = int(input("You wish to make another entry? Enter '1' for YES and '0' for NO."))
-
+                another_book = int(input("You wish to make another entry? Enter '1' for YES and '0' for NO."))
     
-    if choice ==2:
-        print("Here's our Authors catalogue:")
-        for row in author_names:
-            print("|\t",row[0]," |\t", row[1]," |\t", row[2])
-            print("________________________________________________")
+
+
+    elif choice == MainChoice.Authors:
+        print("""
+        1. Browse through the Author catalogue.
+        2. Add Authors to the catalogue.
+		""")
+        
+        author_input = int(input("Enter your choice as '1' or '2': "))
+        
+        if author_input == 1:
+           
+            print("Here's our Authors catalogue:")
+            for row in author_names:
+                author = Author(row[0],row[1],row[2])
+                author.print_authors()
+                
+        elif author_input == 2:
+            while another_author!=0:
+                while count < author_row_count:
+                    user_in = input("Enter your entries for Authors: ")
+                    author_list.append(user_in)
+                    count+=1
+                
+                with open('authors.csv','a')as csvfile:
+                    writer = csv.writer(csvfile)
+                    writer.writerow(author_list)
+                
+                another_author = int(input("You wish to make another entry? Enter '1' for YES and '0' for NO."))
+
+
+
+    elif choice == MainChoice.BookReviews:
+        print("""
+        1. Browse through the Review Section.
+        2. Add new Book Reviews to the catalogue.
+		""")
+        
+        review_input = int(input("Enter your choice as '1' or '2': "))
+        
+        if review_input == 1:
+           
+            print("Here's our Review Section:")
+            for row in book_reviews:
+                review = Review(row[0], row[1], row[2])
+                review.print_reviews()
+                
+        elif review_input == 2:
+            while another_review != 0:
+                while count < review_row_count:
+                    user_in = input("Enter your entries for Reviews: ")
+                    review_list.append(user_in)
+                    count+=1
+                
+                with open('reviews.csv','a')as csvfile:
+                    writer = csv.writer(csvfile)
+                    writer.writerow(review_list)
+                
+                another_review = int(input("You wish to make another entry? Enter '1' for YES and '0' for NO."))
+
+    else:
+        print("Enter a valid choice.")    
 
 
     
--- models.py
+++ models.py
@@ -1,9 +1,13 @@
+
 class Book(object):
     def __init__(self, id, name, author_id, year_of_publication):
         self.id = id
         self.name = name
         self.author_id = author_id
         self.year_of_publication = year_of_publication
+    
+    def print_books(self):
+        print(self.id,"\t", self.name," \t", self.author_id, " \t", self.year_of_publication)
 
 
 class Author(object):
@@ -12,10 +16,33 @@ class Author(object):
         self.first_name = first_name
         self.last_name = last_name
 
+    def print_authors(self):
+        print(self.id, "\t", self.first_name, " \t", self.last_name)
+
 
 class Review(object):
-    def __init__(self, id, book_id, rating, review):
+    def __init__(self, id, book_name, rating):
         self.id = id
-        self.book_id = book_id
+        self.book_name = book_name
         self.rating = rating
-        self.review = review
+
+    def print_reviews(self):
+        print(self.id,  "\t", self.book_name,  "\t", self.rating)
+
+#EMURATED VARIABLES FOR USER CHOICE
+class MainChoice:
+    Books = 1
+    Authors = 2
+    BookReviews = 3
+
+
+def print_messages():
+    print("----------Welcome to the book store!----------")
+    print("________________________________________________\n")
+    
+    print("Here's our catalogue to explore around:")
+    print("""
+    1. Books
+    2.Authors
+    3.Book Reviews
+    """)
